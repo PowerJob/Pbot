6256c763f2f1a3107f9a35f30b3a1c61344b06b7
tjq <tengjiqi@gmail.com>
2023-01-24 16:30:37 +0800

    INIT_PROJECT

---
+++ pbot/src/main/java/tech/powerjob/pbot/PbotApplication.java
@@ -0,0 +1,13 @@
+package tech.powerjob.pbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PbotApplication.class, args);
+    }
+
+}
---
+++ pbot/src/test/java/tech/powerjob/pbot/PbotApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.powerjob.pbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
