3abaed40677b16e62d181ba8bd93556c9fad42d5
tjq <tengjiqi@gmail.com>
2023-03-17 00:47:42 +0800

    feat: pbot

--- pbot/dev/deploy_normal.sh
+++ pbot/dev/deploy_normal.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 # 部署守护程序
-read -r -p "native image?(Y or N):" native
+cd `dirname $0`/.. || exit
 echo "================== 关闭老应用 =================="
 docker stop pbot
 echo "================== 删除老容器 =================="
 docker container rm pbot
 echo "================== 删除老镜像 =================="
-docker rmi -f tjqq/pbot:latest
+docker rmi -f powerjob/pbot:latest
+echo "================== 构建 jar =================="
+mvn clean package -DskipTests
+echo "================== 拷贝 jar =================="
+/bin/cp target/*.jar dev/pbot.jar
+ls -l dev/pbot.jar
 echo "================== 重新构建 Docker =================="
-if [ "$native" = "y" ] || [  "$native" = "Y" ]; then
-  docker build -f Dockerfile_native -t tjqq/pbot:latest . || exit
-else
-  docker build -t tjqq/pbot:latest . || exit
-fi
+docker build -t powerjob/pbot:latest dev/. || exit
+
 echo "================== 准备启动 PBot =================="
 docker run -d \
        --restart=always \
        --net=host \
        --name pbot \
-       -m 256M \
-       --memory-swap 384M \
+       -m 384M \
+       --memory-swap 512M \
        -p 7777:7777 \
        -e PARAMS="--spring.profiles.active=production" \
        -e JVMOPTIONS="-server " \
        -v ~/powerjob-data/pbot:/root \
-       tjqq/pbot:latest
+       powerjob/pbot:latest
 
 #-m,--memory                  内存限制，格式是数字加单位，单位可以为 b,k,m,g。最小为 4M
 #--memory-swap                内存+交换分区大小总限制。格式同上。必须必-m设置的大
